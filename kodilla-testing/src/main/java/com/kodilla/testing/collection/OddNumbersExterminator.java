1ebf24b3746f29f6f41a2cca530135d46520b0d0
Lucyna.Bluszcz <lucynabluszcz@gmail.com>
2018-02-20 21:02:21 +0100

    bugs fixed test

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,4 +1,17 @@
 package com.kodilla.testing.collection;
 
+import java.util.ArrayList;
+
 public class OddNumbersExterminator {
+    public ArrayList<Integer> exterminate(ArrayList<Integer> numbers) {
+        int temporaryValue = 0;
+        ArrayList<Integer> evenNumbers = new ArrayList<Integer>();
+        for (int n = 0; n < numbers.size(); n++) {
+            temporaryValue = numbers.get(n);
+            if (temporaryValue % 2 == 0) {
+                evenNumbers.add(n);
+            }
+        }
+        return evenNumbers;
+    }
 }
