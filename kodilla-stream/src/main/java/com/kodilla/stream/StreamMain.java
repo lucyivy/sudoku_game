1c3a187b0d08befbd3933cdd18281ac826a10d7e
Lucyna.Bluszcz <lucynabluszcz@gmail.com>
2018-03-18 14:50:11 +0100

    7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.sun.xml.internal.ws.util.StringUtils;
 
 public class StreamMain {
@@ -10,5 +11,8 @@ public class StreamMain {
         poemBeautifier.beautify("dark side", (text) -> text.toUpperCase());
         poemBeautifier.beautify("   something, something, something  ", (text) -> text.trim());
         poemBeautifier.beautify("complete", (text) -> StringUtils.capitalize(text));
-        }
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
+    }
 }
